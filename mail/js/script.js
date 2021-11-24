5256a66ecf27471848d3edd63402c3422ef113eb
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2021-11-24 14:02:35 +0100

    add-mail-script

---
+++ mail/js/script.js
@@ -0,0 +1,25 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+const user_mail = prompt("inserisci la tua email");
+
+const lista_utenti = ["baltazar@yahoo.it" , "gianfilippo@yahoo.it", "romualdo@yahoo.it" , "gerry@yahoo.it"];
+console.log (lista_utenti);
+
+
+
+for(i = 0; i < lista_utenti.length; i++) {
+    if (user_mail == lista_utenti[i]) {
+        console.log("sei nella lista");   
+
+}   else if (user_mail != lista_utenti[i]) {
+            console.log("non sei nella lista");
+    }
+}
+
+
+
+
+
