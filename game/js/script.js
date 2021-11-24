700219b8620e641998389d1192c19941c9eafa32
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2021-11-24 15:20:06 +0100

    end game

--- game/js/script.js
+++ game/js/script.js
@@ -1,16 +1,24 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+
+const container = document.querySelector ('.container');
+
+const cont_result = document.querySelector ('.cont_result');
+cont_result.className = "container";
+cont_result.style.color = "red";
+
 let rand_player = Math.floor(Math.random() * 7);
-console.log (rand_player);
+container.innerHTML += (`<h1>Il tuo numero: ${rand_player}<\h1>`);
 
 let rand_pc = Math.floor(Math.random() * 7);
-console.log(rand_pc);
+container.innerHTML += (`<h1>Il numero del pc: ${rand_pc}<\h1>`);
 
 if (rand_player < rand_pc) {
-    console.log ("Ha vinto il computer");
+     cont_result.innerHTML = `<h1>Ha vinto il pc<\h1>`;
+    
 } else if (rand_player > rand_pc){
-    console.log ("ha vinto il player");
+    cont_result.innerHTML = `<h1>Ha vinto il player<\h1>`;
 } else {
-    console.log ("Parità");
+    cont_result.innerHTML = `<h1>Parità<\h1>`;
 }
